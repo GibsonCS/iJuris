74bdbd85ac8837c10926b17ea4cd0ced47d6d79e
GibsonCS <gibson.cruzds@gmail.com>
2026-06-15 19:58:48 -0300

    feat: first commit

---
+++ src/main/java/br/com/ijuris/IJurisApplication.java
@@ -0,0 +1,13 @@
+package br.com.ijuris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IJurisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IJurisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ijuris/IJurisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ijuris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IJurisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
