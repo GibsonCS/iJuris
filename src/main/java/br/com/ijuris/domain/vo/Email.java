a9400182394cb4e6f7c864bb2336f2d508c77a50
GibsonCS <gibson.cruzds@gmail.com>
2026-06-16 19:01:21 -0300

    feat(domain): create value object email

--- src/main/java/br/com/ijuris/domain/vo/Email.java
+++ src/main/java/br/com/ijuris/domain/vo/Email.java
@@ -1,2 +1,14 @@
-package br.com.ijuris.domain.vo;public class Email {
+package br.com.ijuris.domain.vo;
+
+import br.com.ijuris.exception.BusinessException;
+
+public record Email(String email) {
+    public Email {
+
+        String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+
+        if (email == null || !email.matches(regex)) {
+            throw new BusinessException("E-mail em formato inválido.");
+        }
+    }
 }
