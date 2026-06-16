93fffbad7a72a91472c7ce0e5e19e5080b97509c
GibsonCS <gibson.cruzds@gmail.com>
2026-06-16 19:43:21 -0300

    feat(domain): implement validation age

--- src/main/java/br/com/ijuris/domain/User.java
+++ src/main/java/br/com/ijuris/domain/User.java
@@ -5,6 +5,7 @@ import br.com.ijuris.domain.vo.Email;
 import br.com.ijuris.exception.BusinessException;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.UUID;
 
 public class User {
@@ -26,15 +27,26 @@ public class User {
         this.dataNascimento = dataNascimento;
     }
 
-    public static User create(UUID id, String nome, String sobrenome, String cpf, String email, LocalDate dataNascimento) {
+    public static User create(UUID id, String nome, String sobrenome, String cpf, String email,
+                              LocalDate dataNascimento
+    ) {
         validarNome(nome);
         validarNome(sobrenome);
+        validateAge(dataNascimento);
         return new User(id, nome, sobrenome, new Cpf(cpf), new Email(email), dataNascimento);
     }
 
     private static void validarNome(String name) {
         if (name.isBlank()) {
             throw new BusinessException("Nome ou sobrenome não pode estar vazio!");
+        }
+    }
+
+    private static void validateAge(LocalDate dataNascimento) {
+        int age = Period.between(dataNascimento, LocalDate.now()).getYears();
+
+        if (age < 18) {
+            throw new BusinessException("Você deve ter 18 anos ou mais para usar nossos serviços.");
         }
     }
 
