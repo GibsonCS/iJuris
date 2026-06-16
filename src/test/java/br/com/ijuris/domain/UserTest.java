a32179f849390d28afc836515c96b240342aa696
GibsonCS <gibson.cruzds@gmail.com>
2026-06-15 22:41:02 -0300

    test(domain): should create user

--- src/test/java/br/com/ijuris/domain/UserTest.java
+++ src/test/java/br/com/ijuris/domain/UserTest.java
@@ -1,4 +1,27 @@
-import static org.junit.jupiter.api.Assertions.*;
+package br.com.ijuris.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
 class UserTest {
-  
+
+    private UUID VALID_ID = UUID.randomUUID();
+    private String VALID_NAME = "Gibson";
+    private String VALID_SOBRENOME = "CRUZ";
+    private String VALID_CPF = "156.753.577-18";
+    private String VALID_EMAIL = "gibson8fla@gmail.com";
+    private LocalDate VALID_DATA_NASCIMENTO = LocalDate.of(1996, 9, 22);
+
+    @Test
+    void shouldCreateANewUser() {
+
+        User userCreated = User.create(VALID_ID, VALID_NAME, VALID_SOBRENOME, VALID_CPF, VALID_EMAIL,
+                VALID_DATA_NASCIMENTO
+        );
+
+        Assertions.assertEquals(VALID_ID, userCreated.getId());
+    }
 }
