2f0e655c416b8c51c5eef639f8383957ed2d209d
GibsonCS <gibson.cruzds@gmail.com>
2026-06-15 22:40:32 -0300

    feat(domain): implement create user

---
+++ src/main/java/br/com/ijuris/domain/User.java
@@ -0,0 +1,52 @@
+package br.com.ijuris.domain;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class User {
+
+    private final UUID id;
+    private final String nome;
+    private final String sobrenome;
+    private final String cpf;
+    private final String email;
+    private final LocalDate dataNascimento;
+
+    private User(UUID id, String nome, String sobrenome, String cpf, String email, LocalDate dataNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public static User create(UUID id, String nome, String sobrenome, String cpf, String email, LocalDate dataNascimento) {
+
+        return new User(id, nome, sobrenome, cpf, email, dataNascimento);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+}
