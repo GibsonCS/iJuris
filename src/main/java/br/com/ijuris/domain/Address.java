9ce9b1578578ad3e13178ce50a6981bf542b0671
GibsonCS <gibson.cruzds@gmail.com>
2026-06-16 21:07:54 -0300

    feat(domain): implement address entity

--- src/main/java/br/com/ijuris/domain/Address.java
+++ src/main/java/br/com/ijuris/domain/Address.java
@@ -1,2 +1,78 @@
-package br.com.ijuris.domain;public class Address {
+package br.com.ijuris.domain;
+
+import br.com.ijuris.domain.vo.Cep;
+
+import java.util.UUID;
+
+public class Address {
+
+    private final UUID id;
+    private final UUID IdUsuario;
+
+    private Cep cep;
+    private String estado;
+    private String cidade;
+    private String bairro;
+    private String numero;
+    private String complemento;
+    private String lograduouro;
+
+    public Address(UUID id, UUID idUsuario, Cep cep, String estado, String cidade, String bairro, String numero,
+                   String complemento, String lograduouro
+    ) {
+        this.id = id;
+        this.IdUsuario = idUsuario;
+        this.cep = cep;
+        this.estado = estado;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.lograduouro = lograduouro;
+    }
+
+    public static Address create(UUID idUsuario, Cep cep, String estado, String cidade, String bairro,
+                                 String numero, String complemento, String lograduouro
+    ) {
+
+        return new Address(UUID.randomUUID(), idUsuario, cep, estado, cidade, bairro, numero, complemento,
+                lograduouro
+        );
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public UUID getIdUsuario() {
+        return IdUsuario;
+    }
+
+    public Cep getCep() {
+        return cep;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public String getLograduouro() {
+        return lograduouro;
+    }
 }
