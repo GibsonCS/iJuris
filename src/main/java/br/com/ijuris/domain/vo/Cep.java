8c2f8b76b2348f31ba2dd76e33cf91675772e5df
GibsonCS <gibson.cruzds@gmail.com>
2026-06-16 21:06:31 -0300

    feat(domain): implement object value cep

---
+++ src/main/java/br/com/ijuris/domain/vo/Cep.java
@@ -0,0 +1,19 @@
+package br.com.ijuris.domain.vo;
+
+import br.com.ijuris.exception.BusinessException;
+
+public record Cep(String cep) {
+    public Cep {
+
+        String regex = "\\d{5}-\\d{3}";
+
+        if (!cep.matches(regex)) {
+            throw new BusinessException("Insira um cep válido");
+        }
+    }
+
+    @Override
+    public String cep() {
+        return cep;
+    }
+}
