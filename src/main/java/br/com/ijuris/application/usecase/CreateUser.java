bf251fd082c811f053c8fe213b396e219f3b300c
GibsonCS <gibson.cruzds@gmail.com>
2026-06-19 14:57:20 -0300

    feat(application): implement create user

--- src/main/java/br/com/ijuris/application/usecase/CreateUser.java
+++ src/main/java/br/com/ijuris/application/usecase/CreateUser.java
@@ -1,30 +1,55 @@
-package br.com.ijuris.domain.application.usecase;
+package br.com.ijuris.application.usecase;
 
-import br.com.ijuris.domain.application.dto.CreateUserInput;
+import br.com.ijuris.application.dto.CreateUserInput;
 import br.com.ijuris.domain.entity.Address;
+import br.com.ijuris.domain.entity.Role;
 import br.com.ijuris.domain.entity.User;
 import br.com.ijuris.domain.repository.AddressRepository;
+import br.com.ijuris.domain.repository.RoleRepository;
 import br.com.ijuris.domain.repository.UserRepository;
 import br.com.ijuris.exception.BusinessException;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public class CreateUser {
+
+    private static final UUID DEFAULT_CUSTOMER_ROLE_ID = UUID.fromString("125ad5a3-d23e-4878-992b-c8e3bc112b20");
 
     private final UserRepository userRepository;
     private final AddressRepository addressRepository;
+    private final RoleRepository roleRepository;
 
-    public CreateUser(UserRepository userRepository, AddressRepository addressRepository) {
+    public CreateUser(
+            UserRepository userRepository,
+            AddressRepository addressRepository,
+            RoleRepository roleRepository
+    ) {
         this.userRepository = userRepository;
         this.addressRepository = addressRepository;
+        this.roleRepository = roleRepository;
     }
 
-    User execute(CreateUserInput createUserInput) {
+    public User execute(CreateUserInput createUserInput) {
 
         userRepository.findByEmail(createUserInput.email()).ifPresent(user -> {
-            throw new BusinessException("Erro ao criar usuário");
+            throw new BusinessException("O e-mail informado já está cadastrado.");
         });
 
-        User createdUser = User.create(createUserInput.nome(), createUserInput.sobrenome(), createUserInput.cpf(),
-                createUserInput.email(), createUserInput.dataNascimento());
+        Optional<Role> customerRole = roleRepository.findById(DEFAULT_CUSTOMER_ROLE_ID);
+
+        if (customerRole.isEmpty()) {
+            throw new BusinessException("A Role de cliente padrão não foi encontrada.");
+        }
+
+        User createdUser = User.create(
+                createUserInput.nome(),
+                createUserInput.sobrenome(),
+                createUserInput.cpf(),
+                createUserInput.email(),
+                createUserInput.dataNascimento(),
+                customerRole.get()
+        );
 
         Address createdAddress = Address.create(
                 createdUser.getId(),
@@ -34,11 +59,10 @@ public class CreateUser {
                 createUserInput.addressInput().bairro(),
                 createUserInput.addressInput().numero(),
                 createUserInput.addressInput().complemento(),
-                createUserInput.addressInput().lograduouro()
+                createUserInput.addressInput().logradouro()
         );
 
         userRepository.save(createdUser);
-
         addressRepository.save(createdAddress);
 
         return createdUser;
