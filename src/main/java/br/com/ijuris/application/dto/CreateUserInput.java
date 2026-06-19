82a47523e0f2687f29c2eebf377411d6ee2484b0
GibsonCS <gibson.cruzds@gmail.com>
2026-06-19 14:54:57 -0300

    feat(application): create user dto

--- src/main/java/br/com/ijuris/application/dto/CreateUserInput.java
+++ src/main/java/br/com/ijuris/application/dto/CreateUserInput.java
@@ -2,7 +2,12 @@ package br.com.ijuris.application.dto;
 
 import java.time.LocalDate;
 
-public record CreateUserInput(String nome, String sobrenome, String cpf, String email, LocalDate dataNascimento,
-                              AddressInput addressInput
+public record CreateUserInput(
+        String nome,
+        String sobrenome,
+        String cpf,
+        String email,
+        LocalDate dataNascimento,
+        AddressInput addressInput
 ) {
 }
