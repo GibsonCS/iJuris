751015ebfba0351f17097e728a569556a660fe36
GibsonCS <gibson.cruzds@gmail.com>
2026-06-21 17:53:40 -0300

    refactor(infra): address request dto

--- src/main/java/br/com/ijuris/infrastructure/api/dto/AddressRequestDTO.java
+++ src/main/java/br/com/ijuris/infrastructure/api/dto/AddressRequestDTO.java
@@ -1,6 +1,28 @@
 package br.com.ijuris.infrastructure.api.dto;
 
-public record AddressRequestDTO(String cep, String estado, String cidade, String bairro, String numero,
-                                String complemento, String logradouro
+import jakarta.validation.constraints.NotBlank;
+
+public record AddressRequestDTO(
+
+        @NotBlank(message = "O cep é obrigatório")
+        String cep,
+
+        @NotBlank(message = "O estado é obrigatório")
+        String state,
+
+        @NotBlank(message = "A cidade é obrigatória")
+        String city,
+
+        @NotBlank(message = "O bairro é obrigatório")
+        String neighborhood,
+
+        @NotBlank(message = "O número é obrigatório")
+        String number,
+
+        @NotBlank(message = "O complemento é obrigatório")
+        String complement,
+
+        @NotBlank(message = "A rua é obrigatória")
+        String street
 ) {
 }
