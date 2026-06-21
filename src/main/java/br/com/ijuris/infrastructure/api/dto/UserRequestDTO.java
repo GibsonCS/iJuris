f8e44f3d28e948cb61b2f0f2e83e1fcad5d044fb
GibsonCS <gibson.cruzds@gmail.com>
2026-06-21 17:38:50 -0300

    refactor(infra): add validations in user dto

--- src/main/java/br/com/ijuris/infrastructure/api/dto/UserRequestDTO.java
+++ src/main/java/br/com/ijuris/infrastructure/api/dto/UserRequestDTO.java
@@ -1,6 +1,26 @@
 package br.com.ijuris.infrastructure.api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDate;
 
-public record UserRequestDTO(String nome, String sobrenome, String cpf, String email, LocalDate dataNascimento) {
+public record UserRequestDTO(
+
+        @NotBlank(message = "O nome não deve etar em branco.")
+
+        String name,
+        @NotBlank(message = "Sobrenome é obrigatório")
+        String lastname,
+
+        @NotBlank(message = "CPF é obrigatório")
+        String cpf,
+
+        @NotBlank(message = "O email é obrigatório")
+        @Email(message = "Email inválido")
+        String email,
+
+        @NotBlank(message = "A data de nascimento é obrigatória")
+        LocalDate dateOfBirthday
+) {
 }
