db77e06d30905f744cec96903b85d2609980a287
GibsonCS <gibson.cruzds@gmail.com>
2026-06-21 18:39:48 -0300

    feat(core): create role repository find by name

--- src/main/java/br/com/ijuris/core/domain/repository/RoleRepository.java
+++ src/main/java/br/com/ijuris/core/domain/repository/RoleRepository.java
@@ -1,10 +1,12 @@
 package br.com.ijuris.core.domain.repository;
 
 import br.com.ijuris.core.domain.entity.Role;
+import br.com.ijuris.infrastructure.persistence.entity.RoleDbEntity;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface RoleRepository {
     Optional<Role> findById(UUID id);
+    Optional<Role> findByName(String name);
 }
