fa5306b8e3a12075b4b7cb98ec19335bd3698cb6
GibsonCS <gibson.cruzds@gmail.com>
2026-06-24 00:26:04 -0300

    feat(infra): add find by email in spring repository

--- src/main/java/br/com/ijuris/infrastructure/persistence/repository/SpringDataUserRepository.java
+++ src/main/java/br/com/ijuris/infrastructure/persistence/repository/SpringDataUserRepository.java
@@ -3,7 +3,9 @@ package br.com.ijuris.infrastructure.persistence.repository;
 import br.com.ijuris.infrastructure.persistence.entity.UserDbEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SpringDataUserRepository extends JpaRepository<UserDbEntity, UUID> {
+    Optional<UserDbEntity> findByEmail(String email);
 }
