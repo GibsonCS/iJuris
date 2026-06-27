30f33734dd877525e90d62af10f325a93afdfe29
GibsonCS <gibson.cruzds@gmail.com>
2026-06-27 00:10:03 -0300

    feat(infra): create bean change email usecase

--- src/main/java/br/com/ijuris/infrastructure/config/UseCaseConfig.java
+++ src/main/java/br/com/ijuris/infrastructure/config/UseCaseConfig.java
@@ -1,7 +1,9 @@
 package br.com.ijuris.infrastructure.config;
 
 import br.com.ijuris.core.application.port.PasswordEncoder;
+import br.com.ijuris.core.application.port.in.ChangeEmailUseCase;
 import br.com.ijuris.core.application.port.in.UpdateUserProfileUseCase;
+import br.com.ijuris.core.application.usecase.ChangeEmailInteractor;
 import br.com.ijuris.core.application.usecase.CreateUser;
 import br.com.ijuris.core.application.usecase.UpdateUserProfileInteractor;
 import br.com.ijuris.core.domain.repository.AddressRepository;
@@ -27,11 +29,16 @@ public class UseCaseConfig {
 
     @Bean
     public CreateUser createUser() {
-        return new CreateUser(userRepository,addressRepository,roleRepository, passwordEncoder);
+        return new CreateUser(userRepository, addressRepository, roleRepository, passwordEncoder);
     }
 
     @Bean
     public UpdateUserProfileUseCase updateUserProfileUseCase() {
         return new UpdateUserProfileInteractor(userRepository);
+    }
+
+    @Bean
+    public ChangeEmailUseCase changeEmailUseCase() {
+        return new ChangeEmailInteractor(userRepository);
     }
 }
