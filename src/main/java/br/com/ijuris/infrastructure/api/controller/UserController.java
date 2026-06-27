48a5d42853f595360743a159ebe6d49203d194fa
GibsonCS <gibson.cruzds@gmail.com>
2026-06-27 00:10:41 -0300

    feat(infra): create change email controller in user controller

--- src/main/java/br/com/ijuris/infrastructure/api/controller/UserController.java
+++ src/main/java/br/com/ijuris/infrastructure/api/controller/UserController.java
@@ -2,13 +2,11 @@ package br.com.ijuris.infrastructure.api.controller;
 
 import br.com.ijuris.core.application.dto.CreateUserInput;
 import br.com.ijuris.core.application.dto.UpdateUserInput;
+import br.com.ijuris.core.application.port.in.ChangeEmailUseCase;
 import br.com.ijuris.core.application.port.in.UpdateUserProfileUseCase;
 import br.com.ijuris.core.application.usecase.CreateUser;
 import br.com.ijuris.core.domain.entity.User;
-import br.com.ijuris.infrastructure.api.dto.CreateUserRequestDTO;
-import br.com.ijuris.infrastructure.api.dto.CreateUserResponseDTO;
-import br.com.ijuris.infrastructure.api.dto.UpdateUserRequestDTO;
-import br.com.ijuris.infrastructure.api.dto.UpdateUserResponseDTO;
+import br.com.ijuris.infrastructure.api.dto.*;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,10 +20,12 @@ public class UserController {
 
     private final CreateUser createUser;
     private final UpdateUserProfileUseCase updateUserProfileUseCase;
+    private final ChangeEmailUseCase changeEmailUseCase;
 
-    public UserController(CreateUser createUser, UpdateUserProfileUseCase updateUserProfileUseCase) {
+    public UserController(CreateUser createUser, UpdateUserProfileUseCase updateUserProfileUseCase, ChangeEmailUseCase changeEmailUseCase) {
         this.createUser = createUser;
         this.updateUserProfileUseCase = updateUserProfileUseCase;
+        this.changeEmailUseCase = changeEmailUseCase;
     }
 
     @PostMapping
@@ -45,8 +45,19 @@ public class UserController {
 
         UpdateUserInput updateUserInput = request.toInput();
 
-        User updatedUser = updateUserProfileUseCase.execute(id,updateUserInput);
+        User updatedUser = updateUserProfileUseCase.execute(id, updateUserInput);
 
         return ResponseEntity.status(HttpStatus.OK).body(UpdateUserResponseDTO.fromDomain(updatedUser));
+    }
+
+    @PatchMapping("/{id}/email")
+    public ResponseEntity<ChangeEmailResponseDTO> handleChangeEmail(
+            @RequestBody
+            @Valid ChangeEmailRequestDTO request,
+            @PathVariable UUID id
+    ) {
+        String updatedEmail = changeEmailUseCase.execute(id, request.email());
+
+        return ResponseEntity.status(HttpStatus.OK).body(ChangeEmailResponseDTO.fromDomain(updatedEmail));
     }
 }
